fd0d8b9fd106e501f321bc9c567474de6bf6cba0
nutrimoon <97510107+nutrimoon@users.noreply.github.com>
2022-02-22 12:35:44 +0600

    Add files via upload

---
+++ mouseout.js
@@ -0,0 +1,49 @@
+document.getElementsByTagName("img")[4].onmouseout = function() {mouseOut1()};
+document.getElementsByTagName("img")[5].onmouseout = function() {mouseOut2()};
+document.getElementsByTagName("img")[6].onmouseout = function() {mouseOut3()};
+document.getElementsByTagName("img")[7].onmouseout = function() {mouseOut4()};
+document.getElementsByTagName("img")[8].onmouseout = function() {mouseOut5()};
+document.getElementsByTagName("img")[9].onmouseout = function() {mouseOut6()};
+document.getElementsByTagName("img")[10].onmouseout = function() {mouseOut7()};
+document.getElementsByTagName("img")[11].onmouseout = function() {mouseOut8()};
+
+function mouseOut1() {
+    var img4 = document.getElementsByTagName("img")[4];
+    img4.setAttribute("src", "img/food/1.PNG");
+    
+  }
+
+  function mouseOut2() {
+    var img5 = document.getElementsByTagName("img")[5];
+    img5.setAttribute("src", "img/food/2.PNG"); 
+  }
+
+  function mouseOut3() {
+    var img6 = document.getElementsByTagName("img")[6];
+    img6.setAttribute("src", "img/food/3.PNG"); 
+  }
+
+  function mouseOut4() {
+    var img7 = document.getElementsByTagName("img")[7];
+    img7.setAttribute("src", "img/food/4.PNG"); 
+  }
+
+  function mouseOut5() {
+    var img8 = document.getElementsByTagName("img")[8];
+    img8.setAttribute("src", "img/food/5.PNG"); 
+  }
+
+  function mouseOut6() {
+    var img9 = document.getElementsByTagName("img")[9];
+    img9.setAttribute("src", "img/food/6.PNG"); 
+  }
+
+  function mouseOut7() {
+    var img10 = document.getElementsByTagName("img")[10];
+    img10.setAttribute("src", "img/food/7.PNG"); 
+  }
+
+  function mouseOut8() {
+    var img11 = document.getElementsByTagName("img")[11];
+    img11.setAttribute("src", "img/food/8.PNG"); 
+  }
---
+++ mouseover.js
@@ -0,0 +1,49 @@
+document.getElementsByTagName("img")[4].onmouseover = function() {mouseOver1()};
+document.getElementsByTagName("img")[5].onmouseover = function() {mouseOver2()};
+document.getElementsByTagName("img")[6].onmouseover = function() {mouseOver3()};
+document.getElementsByTagName("img")[7].onmouseover = function() {mouseOver4()};
+document.getElementsByTagName("img")[8].onmouseover = function() {mouseOver5()};
+document.getElementsByTagName("img")[9].onmouseover = function() {mouseOver6()};
+document.getElementsByTagName("img")[10].onmouseover = function() {mouseOver7()};
+document.getElementsByTagName("img")[11].onmouseover = function() {mouseOver8()};
+
+function mouseOver1() {
+    var img4 = document.getElementsByTagName("img")[4];
+    img4.setAttribute("src", "img/foodover/1.PNG");
+    
+  }
+
+  function mouseOver2() {
+    var img5 = document.getElementsByTagName("img")[5];
+    img5.setAttribute("src", "img/foodover/2.PNG"); 
+  }
+
+  function mouseOver3() {
+    var img6 = document.getElementsByTagName("img")[6];
+    img6.setAttribute("src", "img/foodover/3.PNG"); 
+  }
+
+  function mouseOver4() {
+    var img7 = document.getElementsByTagName("img")[7];
+    img7.setAttribute("src", "img/foodover/4.PNG"); 
+  }
+
+  function mouseOver5() {
+    var img8 = document.getElementsByTagName("img")[8];
+    img8.setAttribute("src", "img/foodover/5.PNG"); 
+  }
+
+  function mouseOver6() {
+    var img9 = document.getElementsByTagName("img")[9];
+    img9.setAttribute("src", "img/foodover/6.PNG"); 
+  }
+
+  function mouseOver7() {
+    var img10 = document.getElementsByTagName("img")[10];
+    img10.setAttribute("src", "img/foodover/7.PNG"); 
+  }
+
+  function mouseOver8() {
+    var img11 = document.getElementsByTagName("img")[11];
+    img11.setAttribute("src", "img/foodover/8.PNG"); 
+  }
